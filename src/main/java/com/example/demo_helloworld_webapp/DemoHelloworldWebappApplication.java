83f1af8fab8e7e101c77598717228daceee25a58
Srivastava <sarthak.srivastava@conduent.com>
2025-06-21 13:39:54 +0530

    Initial commit: Java app + Dockerfile

---
+++ src/main/java/com/example/demo_helloworld_webapp/DemoHelloworldWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_helloworld_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloworldWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHelloworldWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_helloworld_webapp/DemoHelloworldWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_helloworld_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloworldWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
